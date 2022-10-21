845d3c92b05bba9cbf00d4c91651c5138906be8d
Vitor Dinis <vitor.daniel@puregym.com>
2022-10-21 09:41:53 +0100

    Initial Commit

---
+++ Shopper/ShopperApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShopperApp.swift
+//  Shopper
+//
+//  Created by Vitor Dinis on 21/10/2022.
+//
+
+import SwiftUI
+
+@main
+struct ShopperApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
